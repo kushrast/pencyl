4700f2b1d08f2f87fa7787d64352ffc982f240af
Kush Rastogi <krustey@gmail.com>
2020-04-20 19:58:04 -0700

    Adding basic repo

---
+++ src/main/java/com/kushrastogi/thoughtbank/ThoughtbankApplication.java
@@ -0,0 +1,13 @@
+package com.kushrastogi.thoughtbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushrastogi/thoughtbank/ThoughtbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushrastogi.thoughtbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
