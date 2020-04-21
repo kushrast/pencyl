464f4b0b752c6aec6d72df277f3bd66dd158ff10
Kush Rastogi <krustey@gmail.com>
2020-04-20 23:22:33 -0700

    Returning from components

--- src/main/js/Icon.jsx
+++ src/main/js/Icon.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Pen from './img/pen.svg';
 
 class Icon extends Component {
   render() {
-  	<div className="icon">
-  		<img className="icon-image" src={Pen} alt="Add a note" />;
-  	</div>
+  	return (
+	  	<div className="icon">
+	  		<img className="icon-image" src="/img/pen.svg" alt="Add a note" />;
+	  	</div>
+  	);
   }
 }
 export default Icon;
--- src/main/js/ThoughtCard.jsx
+++ src/main/js/ThoughtCard.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 class ThoughtCard extends Component {
   render() {
-  	<div className="thought-card">
-  		<div className="top-row">
-  			<div className="title">
-  				Title
+  	return (
+  		<div className="thought-card">
+  			<div className="top-row">
+  				<div className="title">
+  					Title
+  				</div>
   			</div>
+  			<div className="content">What's on your mind?</div>
   		</div>
-  		<div className="content">What's on your mind?</div>
-  	</div>
+  		);
   }
 }
 export default ThoughtCard;
