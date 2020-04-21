b053e86cfa78c203f19f59ecbac041ad22950eed
Kush Rastogi <krustey@gmail.com>
2020-04-20 23:34:29 -0700

    Pushing new edits

--- src/main/js/ThoughtCard.jsx
+++ src/main/js/ThoughtCard.jsx
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 class ThoughtCard extends Component {
   render() {
   	return (
-  		<div className="thought-card margin-add">
-  			<div className="top-row">
-  				<div className="title">
-  					Title
-  				</div>
-  			</div>
-  			<div className="content">What's on your mind?</div>
+  		<div className="thought-card-container thought-card-margin-add">
+	  		<div className="thought-card">
+	  			<div className="top-row">
+	  				<div className="thought-title">
+	  					Title
+	  				</div>
+	  			</div>
+	  			<div className="thought-content">What's on your mind?</div>
+	  		</div>
   		</div>
+  		
   		);
   }
 }
