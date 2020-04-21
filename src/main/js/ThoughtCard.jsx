bb4f4f918de5be3e0aaa492f113eabb55390eefb
Kush Rastogi <krustey@gmail.com>
2020-04-20 22:23:28 -0700

    Including css

--- src/main/js/ThoughtCard.jsx
+++ src/main/js/ThoughtCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./css/app.css";
 
 class ThoughtCard extends Component {
   render() {
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import ThoughtCard from './ThoughtCard.jsx';
+import "./css/app.css";
 
 const ReactDOM = require('react-dom');
 const client = require('./client');
@@ -8,19 +9,13 @@ class App extends Component {
 
 	constructor(props) {
 		super(props);
-		// this.state = {employees: []};
 	}
-
-	// componentDidMount() {
-	// 	client({method: 'GET', path: '/api/employees'}).done(response => {
-	// 		this.setState({employees: response.entity._embedded.employees});
-	// 	});
-	// }
 
 	render() {
 		return (
-			// <EmployeeList employees={this.state.employees}/>
-			<ThoughtCard />
+			<div class="container">
+				<ThoughtCard />
+			</div>
 		);
 	}
 }
